b3cd28be15f77bde6976dac2bcb014b124050cbc
Hardik Gondaliya <hardikg23@gmail.com>
2019-08-31 11:48:24 +0530

    purches page final changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def get_conversion_rate(payments)
+    total_o = 0
+    total = 0
+    payments.each do |p|
+      total_o = total_o + (p.amount*p.conversion_rate) if p.amount_type == 'dollar'
+      total = total + p.amount
+    end
+    return (total_o/total).round(2)
+  end
+
 end
