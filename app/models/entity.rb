5271afaf075a78f2a664baeac02c85054b3abc52
Hardik Gondaliya <hardikg23@gmail.com>
2019-08-31 09:57:49 +0530

    removing mobile number

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -5,8 +5,8 @@ class Entity < ApplicationRecord
 
   validates_presence_of  :name
   validates_length_of :name,    :within => 5..50
-  validates_length_of :mobile,    :is => 10
-  validates_format_of :mobile, :with => /\A[0-9]+\Z/, :message => " : Please enter correct mobile No"
+  validates_length_of :mobile,    :is => 10, :allow_blank => true
+  validates_format_of :mobile, :with => /\A[0-9]+\Z/, :message => " : Please enter correct mobile No", :allow_blank => true
 
   def get_entity_name_for_select_options
     "#{name} (#{mobile})"
